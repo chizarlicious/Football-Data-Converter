aae46cb9c97de95813f663e8ccc3083ca07e2dc2
Alexander Gude <alex.public.account@gmail.com>
2013-12-07 09:51:33 -0600

    Add unittest and skeleton for convert_weather

--- code/parse_raw_data/parse_gameinfo.py
+++ code/parse_raw_data/parse_gameinfo.py
@@ -9,5 +9,19 @@ def convert_time(time_string):
 
     returns:
         None if no sensible conversion exits, otherwise returns a string of the
-        format '%H:%M'
+            format '%H:%M'
+    """
+
+def convert_weather(weather_string):
+    """Takes in a string describing the weather and returns a dictionary of the
+    information.
+
+    args:
+        weather_string: A string detailing the weather
+
+    returns:
+        A dictionary with keys: ["temperature", "relative humidity", "wind
+            speed", "wind chill"] in Fahrenheit and miles per hour. Relative
+            humidity is a number [0., 1.]. Any missing value is replaced with
+            None. On failure, returns None instead of a dictionary.
     """
