0027a5b13473af3a809bfd8dfd33a9d49b73be7b
Alexander Gude <alex.public.account@gmail.com>
2014-02-09 14:49:45 -0600

    Add __parse_game_info to converter.py
    Add various helper functions used in __parse_game_info to
    parse_game_info.py.
    
    Add tests to cover added functions in parse_game_info.py to
    test_game_info.py.
    
    Add a team name to team code converter in team_list.py.

--- code/tests/test_game_info.py
+++ code/tests/test_game_info.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
-from raw_data_parsers.parse_game_info import convert_time, convert_weather
+from raw_data_parsers.parse_game_info import convert_time, convert_weather, convert_duration, convert_overunder, convert_vegas_line
 
 
 class TestGameInfo(unittest.TestCase):
@@ -10,10 +10,20 @@ class TestGameInfo(unittest.TestCase):
         # Successful
         self.assertEqual(convert_time("09:32am"), "09:32")
         self.assertEqual(convert_time("1:59pm"), "13:59")
-        # Failure raies a ValueError
+        # Failure raises a ValueError
         self.assertRaises(ValueError, convert_time, "The time is one o'clock.")
         self.assertRaises(ValueError, convert_time, "13:40pm")
         self.assertRaises(ValueError, convert_time, "11:65pm")
+
+    def test_convert_duration(self):
+        # Successful
+        self.assertEqual(convert_duration("4:11"), 15060)
+        self.assertEqual(convert_duration("00:11"), 660)
+        self.assertEqual(convert_duration("13:11"), 47460)
+        # Failure raises a ValueError
+        self.assertRaises(ValueError, convert_duration, "The game lasted 2 hours.")
+        self.assertRaises(ValueError, convert_duration, "13:40pm")
+        self.assertRaises(ValueError, convert_duration, "11:65")
 
     def test_convert_weather(self):
         # Successful
@@ -33,9 +43,29 @@ class TestGameInfo(unittest.TestCase):
                 convert_weather("28 degrees, wind 1 mph"),
                 {"temperature": 28, "relative humidity": None, "wind speed": 1, "wind chill": None}
                 )
-        # Failure returns dictionary full of None
+        # Failure returns dictionary full of None, or a ValueError
         self.assertEqual(convert_weather("I have no idea what degree to get!"), None)
         self.assertRaises(ValueError, convert_weather, "twenty-eight degrees, wind five mph")
+
+    def test_convert_overunder(self):
+        # Successful
+        self.assertEqual(convert_overunder("44.5(over)"), 44.5)
+        self.assertEqual(convert_overunder("0 (under)"), 0.)
+        self.assertEqual(convert_overunder("32 (under)"), -32.)
+        # Failure raises a ValueError
+        self.assertRaises(ValueError, convert_overunder, "35 under")
+        self.assertRaises(ValueError, convert_overunder, "-23")
+        self.assertRaises(ValueError, convert_overunder, "five (under)")
+
+    def test_convert_vegas_line(self):
+        # Successful
+        self.assertEqual(convert_vegas_line("Arizona Cardinals -9.0"), ("ARI", -9))
+        self.assertEqual(convert_vegas_line("New York Giants -19.5"), ("NYG", -19.5))
+        # Failure raises various errors
+        self.assertRaises(IndexError, convert_vegas_line, "Minnesota Vikings 10.5")
+        self.assertRaises(IndexError, convert_vegas_line, "San Diego Chargers +4.5")
+        self.assertRaises(ValueError, convert_vegas_line, "San Diego Chargers -four")
+        self.assertRaises(KeyError, convert_vegas_line, "Pallet Town Charizards -10.")
 
 if __name__ == '__main__':
     unittest.main()
